0246639da2cf518ec10947ea619a021510bca8cb
ChyiYaqing <chyiyaqing@gmail.com>
2024-02-04 22:51:00 +0800

    practice ReactDOM

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+//ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+
+ReactDOM.render(<p>Hi, my name is Chyi!</p>, document.getElementById("root"))
